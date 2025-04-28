ccdac57f2e28eb3b8a793e4f015b11e716c88c75
EricChiu147 <88593071+EricChiu147@users.noreply.github.com>
2025-04-29 04:36:30 +0800

    Day3 if

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  {{ message }}
+  <p>
+    Hello World<br>
+  </p>
+  <p v-if="x === 0">
+    x = 0
+  </p>
+  <p v v-else-if="x === 1">
+    x = 1
+  </p>
+  <p v-else>
+    x out of range
+  </p>
 </template>
 
 <script>
@@ -7,7 +18,8 @@ export default {
   name: 'App',
   data() {
     return {
-      message: 'Hello World',
+      isShow: true,
+      x: 0,
     };
   },
 };
